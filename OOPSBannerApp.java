456a0ba8dfbf183976463ec0b912ef1e2fec1452
ananyab197 <ananya.b1007@gmail.com>
2026-02-18 18:47:43 +0530

    UC4: Implement OOPS banner using Array and Loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -5,56 +5,41 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Store all banner lines in an array
         String[] banner = {
 
             String.join("   ",
-                    "  *****  ",
-                    "  *****  ",
-                    " *****   ",
-                    " *****  "),
+                    " ***** ",
+                    " ***** ",
+                    "*****  ",
+                    " ***** "),
 
             String.join("   ",
-                    " *     * ",
-                    " *     * ",
-                    " *     * ",
-                    " *     * "),
+                    "*     *",
+                    "*     *",
+                    "*    * ",
+                    "*      "),
 
             String.join("   ",
-                    " *     * ",
-                    " *     * ",
-                    " *     * ",
-                    " *     * "),
+                    "*     *",
+                    "*     *",
+                    "*****  ",
+                    " ***** "),
 
             String.join("   ",
-                    "  *****  ",
-                    "  *****  ",
-                    " *****   ",
-                    "  *****  "),
+                    "*     *",
+                    "*     *",
+                    "*      ",
+                    "*     *"),
 
             String.join("   ",
-                    " *       ",
-                    " *       ",
-                    " *       ",
-                    " *       "),
-
-            String.join("   ",
-                    " *       ",
-                    " *       ",
-                    " *       ",
-                    " *       "),
-
-            String.join("   ",
-                    "  *****  ",
-                    " *       ",
-                    " *****   ",
-                    "  *****  ")
+                    " ***** ",
+                    " ***** ",
+                    "*      ",
+                    " ***** ")
         };
 
-        // Print using enhanced for-loop
         for (String line : banner) {
             System.out.println(line);
         }
     }
 }
-
