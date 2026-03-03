1677201b57f1f7e2f33d909e46e64bcf564aaec0
ananyab197 <ananya.b1007@gmail.com>
2026-03-03 10:34:50 +0530

    UC5: Array Initialisation using String.join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,43 +1,17 @@
-/**
-
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
         String[] banner = {
-
-            String.join("   ",
-                    " ***** ",
-                    " ***** ",
-                    "*****  ",
-                    " ***** "),
-
-            String.join("   ",
-                    "*     *",
-                    "*     *",
-                    "*    * ",
-                    "*      "),
-
-            String.join("   ",
-                    "*     *",
-                    "*     *",
-                    "*****  ",
-                    " ***** "),
-
-            String.join("   ",
-                    "*     *",
-                    "*     *",
-                    "*      ",
-                    "*     *"),
-
-            String.join("   ",
-                    " ***** ",
-                    " ***** ",
-                    "*      ",
-                    " ***** ")
+            String.join(" ", "O", "O", "P", "P", "S", "S"),
+            String.join(" ", "O", "O", "P", "P", "S", "S"),
+            String.join(" ", "O", "O", "P", "P", "S", "S"),
+            String.join(" ", "O", "O", "P", "P", "S", "S"),
+            String.join(" ", "O", "O", "P", "P", "S", "S"),
+            String.join(" ", "O", "O", "P", "P", "S", "S"),
+            String.join(" ", "O", "O", "P", "P", "S", "S")
         };
 
+       
         for (String line : banner) {
             System.out.println(line);
         }
