cb924050fd9aa43cac0ab534694ff1f15509f6f0
ananyab197 <ananya.b1007@gmail.com>
2026-03-24 01:07:51 +0530

    UC7: Implememnted Character Pattern class with map-based storage

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,42 +1,68 @@
+import java.util.*;
+
 public class OOPSBanner {
 
-    public static void main(String[] args) {
+   
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
 
-        String[] o = getO();
-        String[] p = getP();
-        String[] s = getS();
 
-        for (int i = 0; i < o.length; i++) {
-            System.out.println(o[i] + "   " + o[i] + "   " + p[i] + "   " + s[i]);
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        // Getter
+        public String[] getPattern() {
+            return pattern;
         }
     }
 
-    
-    public static String[] getO() {
-        return new String[] {
-            " OOO ",
-            "O   O",
-            "O   O",
-            "O   O",
-            " OOO "
-        };
-    }
-    public static String[] getP() {
-        return new String[] {
-            "PPPP ",
-            "P   P",
-            "PPPP ",
-            "P    ",
-            "P    "
-        };
-    }
-    public static String[] getS() {
-        return new String[] {
-            " SSSS",
-            "S    ",
-            " SSS ",
-            "    S",
-            "SSSS "
-        };
+    public static void main(String[] args) {
+
+       
+        CharacterPattern O = new CharacterPattern('O', new String[]{
+                " OOO ",
+                "O   O",
+                "O   O",
+                "O   O",
+                " OOO "
+        });
+
+        CharacterPattern P = new CharacterPattern('P', new String[]{
+                "PPPP ",
+                "P   P",
+                "PPPP ",
+                "P    ",
+                "P    "
+        });
+
+        CharacterPattern S = new CharacterPattern('S', new String[]{
+                " SSSS",
+                "S    ",
+                " SSS ",
+                "    S",
+                "SSSS "
+        });
+
+        
+        Map<Character, CharacterPattern> map = new HashMap<>();
+        map.put('O', O);
+        map.put('P', P);
+        map.put('S', S);
+
+        String word = "OOPS";
+
+        
+        for (int i = 0; i < 5; i++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char c : word.toCharArray()) {
+                line.append(map.get(c).getPattern()[i]).append("   ");
+            }
+
+            System.out.println(line);
+        }
     }
 }
