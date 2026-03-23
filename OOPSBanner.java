19b0599f8ba7499c6b39dee7ffe08fc1747cf1f1
ananyab197 <ananya.b1007@gmail.com>
2026-03-24 00:45:58 +0530

    Added OOPSBanner,java

---
+++ OOPSBanner.java
@@ -0,0 +1,52 @@
+/**
+ 
+ * @author Ananya
+ * @version 3.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("   ",
+                "  *****  ",
+                "  *****  ",
+                " *****   ",
+                " *****  "));
+
+        System.out.println(String.join("   ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * "));
+
+        System.out.println(String.join("   ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * "));
+
+        System.out.println(String.join("   ",
+                "  *****  ",
+                "  *****  ",
+                " *****   ",
+                "  *****  "));
+
+        System.out.println(String.join("   ",
+                " *       ",
+                " *       ",
+                " *       ",
+                " *       "));
+
+        System.out.println(String.join("   ",
+                " *       ",
+                " *       ",
+                " *       ",
+                " *       "));
+
+        System.out.println(String.join("   ",
+                "  *****  ",
+                " *       ",
+                " *****   ",
+                "  *****  "));
+    }
+}
